a30ca3e0373cff22879e84333a9b702b5856fb1c
Juan Ceja <jmc347ls2@yahoo.com>
2022-03-10 13:19:17 -0800

    first commit

---
+++ putting_it_together/src/components/PersonComponent.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+
+class PersonComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    addBirthday = () => {
+        console.log(this.state.age)
+        this.setState({age: this.state.age + 1});
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={ () => this.addBirthday() }>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+            </div>
+        )
+    }
+}
+
+export default PersonComponent
